85162764d41447702b255fb2569265addfda8894
FedeArann02 <aranfederico3@gmail.com>
2024-11-01 15:47:14 -0300

    Calcular parabola de ceros (No imaginarios)

---
+++ ParabolaCeros/frmBhaskara.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace ParabolaCeros
+{
+    partial class frmBhaskara
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBhaskara));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.txtV1 = new System.Windows.Forms.TextBox();
+            this.txtV2 = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pbxSalir = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSalir)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel1.Controls.Add(this.pbxSalir);
+            this.panel1.Controls.Add(this.lblTitulo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(669, 67);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtA
+            // 
+            this.txtA.BackColor = System.Drawing.Color.DarkGray;
+            this.txtA.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA.Location = new System.Drawing.Point(213, 142);
+            this.txtA.MaxLength = 27;
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(242, 38);
+            this.txtA.TabIndex = 1;
+            this.txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtB
+            // 
+            this.txtB.BackColor = System.Drawing.Color.DarkGray;
+            this.txtB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB.Location = new System.Drawing.Point(213, 212);
+            this.txtB.MaxLength = 27;
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(242, 38);
+            this.txtB.TabIndex = 2;
+            this.txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtC
+            // 
+            this.txtC.BackColor = System.Drawing.Color.DarkGray;
+            this.txtC.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC.Location = new System.Drawing.Point(213, 282);
+            this.txtC.MaxLength = 27;
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(242, 38);
+            this.txtC.TabIndex = 3;
+            this.txtC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.IndianRed;
+            this.btnCalcular.FlatAppearance.BorderSize = 0;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI Variable Display", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(262, 352);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(145, 41);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnLimpiar.FlatAppearance.BorderSize = 0;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Segoe UI Variable Display", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpiar.Location = new System.Drawing.Point(262, 399);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(145, 41);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtX1
+            // 
+            this.txtX1.BackColor = System.Drawing.Color.DarkGray;
+            this.txtX1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtX1.Location = new System.Drawing.Point(159, 493);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.ReadOnly = true;
+            this.txtX1.Size = new System.Drawing.Size(159, 38);
+            this.txtX1.TabIndex = 6;
+            this.txtX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtX2
+            // 
+            this.txtX2.BackColor = System.Drawing.Color.DarkGray;
+            this.txtX2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtX2.Location = new System.Drawing.Point(351, 493);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.ReadOnly = true;
+            this.txtX2.Size = new System.Drawing.Size(159, 38);
+            this.txtX2.TabIndex = 7;
+            this.txtX2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtV1
+            // 
+            this.txtV1.BackColor = System.Drawing.Color.DarkGray;
+            this.txtV1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtV1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtV1.Location = new System.Drawing.Point(159, 555);
+            this.txtV1.Name = "txtV1";
+            this.txtV1.ReadOnly = true;
+            this.txtV1.Size = new System.Drawing.Size(159, 38);
+            this.txtV1.TabIndex = 8;
+            this.txtV1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtV2
+            // 
+            this.txtV2.BackColor = System.Drawing.Color.DarkGray;
+            this.txtV2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtV2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtV2.Location = new System.Drawing.Point(351, 555);
+            this.txtV2.Name = "txtV2";
+            this.txtV2.ReadOnly = true;
+            this.txtV2.Size = new System.Drawing.Size(159, 38);
+            this.txtV2.TabIndex = 9;
+            this.txtV2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Segoe UI Variable Display", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblA.Location = new System.Drawing.Point(167, 139);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(40, 41);
+            this.lblA.TabIndex = 10;
+            this.lblA.Text = "A";
+            this.lblA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(167, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 41);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "B";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Variable Display", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(167, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 41);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "C";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Variable Display", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(106, 493);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 41);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "x1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Variable Display", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(516, 493);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 41);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "x2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Variable Display", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(106, 552);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 41);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "v1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Variable Display", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(516, 552);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 41);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "v2";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI Variable Display", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(254, 17);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(161, 36);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "BHASKARA";
+            // 
+            // pbxSalir
+            // 
+            this.pbxSalir.AccessibleDescription = "Salir";
+            this.pbxSalir.AccessibleName = "Salir";
+            this.pbxSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxSalir.Image = ((System.Drawing.Image)(resources.GetObject("pbxSalir.Image")));
+            this.pbxSalir.Location = new System.Drawing.Point(640, 17);
+            this.pbxSalir.Name = "pbxSalir";
+            this.pbxSalir.Size = new System.Drawing.Size(17, 17);
+            this.pbxSalir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxSalir.TabIndex = 2;
+            this.pbxSalir.TabStop = false;
+            this.pbxSalir.Click += new System.EventHandler(this.pbxSalir_Click);
+            // 
+            // frmBhaskara
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(669, 628);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txtV2);
+            this.Controls.Add(this.txtV1);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmBhaskara";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSalir)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.TextBox txtV1;
+        private System.Windows.Forms.TextBox txtV2;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox pbxSalir;
+    }
+}
+
---
+++ ParabolaCeros/frmBhaskara.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParabolaCeros
+{
+    public partial class frmBhaskara : Form
+    {
+        public frmBhaskara()
+        {
+            InitializeComponent();
+            initFade();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            llamarCalcular();
+        }
+
+        private void llamarCalcular()
+        {
+            if (double.TryParse(txtA.Text, out double A) && double.TryParse(txtB.Text, out double B) && double.TryParse(txtC.Text, out double C))
+            {
+                if (A != 0)
+                {
+                    calcularCeros(A, B, C);
+                }
+                else
+                {
+                    MessageBox.Show("A debe ser distinto de cero (A != 0)", "A", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese datos válidos (números enteros o decimales)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void calcularCeros(double A, double B, double C)
+        {
+            double B2, division, op2, result, x1, x2, v1, v2;
+
+            try
+            {
+                B2 = Math.Pow(B, 2);
+                division = 2 * A;
+                op2 = 4 * A * C;
+                result = B2 - op2;
+
+                if (result < 0)
+                {
+                    txtX1.Text = "SIN RAÍZ";
+                    txtX2.Text = "SIN RAÍZ";
+                }
+                else
+                {
+                    result = Math.Sqrt(result);
+                    x1 = (-B + result) / division;
+                    x2 = (-B - result) / division;
+                    txtX1.Text = Convert.ToString(decimal.Round(decimal.Parse(x1.ToString()), 2));
+                    txtX2.Text = Convert.ToString(decimal.Round(decimal.Parse(x2.ToString()), 2));
+                }
+
+                v1 = -(B / division);
+                v2 = A * (Math.Pow(v1, 2)) + B * v1 + C;
+                txtV1.Text = Convert.ToString(decimal.Round(decimal.Parse(v1.ToString()), 2));
+                txtV2.Text = Convert.ToString(decimal.Round(decimal.Parse(v2.ToString()), 2));
+            }
+            catch(Exception ex)
+            {
+                clear();
+                MessageBox.Show("Error: " + ex.Message + "", "Ingrese valores válidos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void clear()
+        {
+            txtA.Text = string.Empty;
+            txtB.Text = string.Empty;
+            txtC.Text = string.Empty;
+            txtV1.Text = string.Empty;
+            txtV2.Text = string.Empty;
+            txtX1.Text = string.Empty;
+            txtX2.Text = string.Empty;
+        }
+
+        private void pbxSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void initFade()
+        {
+            fadeOutTimer = new Timer();
+            fadeOutTimer.Interval = 30; // Tiempo en milisegundos (ajustable para una velocidad de difuminado)
+            fadeOutTimer.Tick += FadeOutTimer_Tick;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (!isClosing)
+            {
+                e.Cancel = true;
+                fadeOutTimer.Start(); // Inicia el difuminado
+            }
+        }
+
+        private void FadeOutTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)
+            {
+                this.Opacity -= 0.15; // Disminuir la opacidad gradualmente
+            }
+            else
+            {
+                fadeOutTimer.Stop(); // Detener el temporizador
+                isClosing = true;
+                this.Close(); // Cerrar el formulario una vez que la opacidad llega a cero
+            }
+        }
+
+        private bool isClosing = false;
+        private Timer fadeOutTimer;
+    }
+}
